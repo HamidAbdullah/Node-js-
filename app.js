bf16a04985522b522f7c99013f477816945619e8
Hamid Abdullah <hamidabdullahofficial@gmail.com>
2024-10-27 00:15:42 -0700

    used validator npm package and check Email Validations

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const fs = require('fs');
-const addTwoNumber = require('./src/service.js')
-const getNotes = require('./src/notes.js')
+// imports
+const validator = require('validator');
+const fs = require("fs");
+const addTwoNumber = require("./src/service.js");
+const getNotes = require("./src/notes.js");
 
+// Creating Variables.
+const notesText = getNotes({ notes: "Hamid ABDULLAH" });
+const sum = addTwoNumber(10, -5);
+const isEmail = validator.isEmail('Hamidabdullahofficial@gmail.com')
+
+// File System Methods
+
+// Write File
 fs.writeFileSync("note.txt", `My name is Hamid Abdullah `);
-fs.appendFileSync('note.txt', " I'm from Pakistan pk ");
+// Append Text in this file.
+fs.appendFileSync("note.txt", " I'm from Pakistan pk ");
 
-const sum = addTwoNumber(10, -5)
-const notesText = getNotes()
-
-console.log({SUM: sum})
-console.log({getNotes: notesText});
+// Log the output
+console.log({isEmail: isEmail})
+console.log({ SUM: sum });
+console.log({ getNotes: notesText });
--- src/notes.js
+++ src/notes.js
@@ -1,5 +1,5 @@
-const getNotes = () => {
-  return "Your Notes";
+const getNotes = ({notes: notes}) => {
+  return `Your Notes ${notes}`;
 };
 
 module.exports = getNotes
