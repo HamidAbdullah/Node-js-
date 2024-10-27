137903752b22af6068e34082b66398e445af6d6f
Hamid Abdullah <hamidabdullahofficial@gmail.com>
2024-10-26 23:57:53 -0700

    Done Udemy First Task

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const fs = require('fs');
 const addTwoNumber = require('./src/service.js')
+const getNotes = require('./src/notes.js')
 
 fs.writeFileSync("note.txt", `My name is Hamid Abdullah `);
 fs.appendFileSync('note.txt', " I'm from Pakistan pk ");
 
 const sum = addTwoNumber(10, -5)
+const notesText = getNotes()
 
 console.log({SUM: sum})
+console.log({getNotes: notesText});
