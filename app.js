daca369bfaa8888defc57687286a7e186737dab8
Hamid Abdullah <hamidabdullahofficial@gmail.com>
2024-10-30 05:28:38 -0700

    use new package yargs , cammand line handler.

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-// imports
-import validator from 'validator';
-import chalk from 'chalk';
-import fs from 'fs';
-import addTwoNumber from './src/service.js';
-import getNotes from './src/notes.js';
+import validator from "validator";
+import chalk from "chalk";
+import fs from "fs";
+import addTwoNumber from "./src/service.js";
+import getNotes from "./src/notes.js";
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';  // This is important for using yargs in ES modules
 
-const log = console.log;
-
-// Combine styled and normal strings
-log(chalk.blue('Hello') + ' World' + chalk.red('!'));
-
-log(chalk.green.inverse('success') + chalk.redBright('!'));
-
-// Creating Variables.
 const notesText = getNotes({ notes: "Hamid ABDULLAH" });
 const sum = addTwoNumber(10, -5);
-const isEmail = validator.isEmail('Hamidabdullahofficial@gmail.com');
+const isEmail = validator.isEmail("Hamidabdullahofficial@gmail.com");
+const log = console.log;
 
-// File System Methods
-
-// Write File
 fs.writeFileSync("note.txt", chalk.green(`My name is Hamid Abdullah `));
-// Append Text in this file.
-fs.appendFileSync("note.txt", " I'm from Pakistan pk ");
+fs.appendFileSync("note.txt", " I'm from Pakistan Abbottabad pk ");
 
-// Log the output using chalk for colored output
-log(chalk.green('Email validation result:'), chalk.greenBright.bgBlueBright(isEmail));
-log(chalk.green('Sum of numbers:'), chalk.yellow.bold(sum));
-log(chalk.green('Notes Text:'), chalk.magenta(notesText));
+log(chalk.red("Email validation result:"), chalk.red(isEmail));
+log(chalk.green("Sum of numbers:"), chalk.yellow.bold(sum));
+log(chalk.green("Notes Text:"), chalk.magenta(notesText));
+log(chalk.blue("Hello") + " World" + chalk.red("!"));
+
+// Properly use yargs with hideBin to parse arguments
+yargs(hideBin(process.argv)).command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note...');
+  }
+}).argv;
+
+yargs(hideBin(process.argv)).command({
+  command: 'remove',
+  describe: 'Removing Node.js',
+  handler: function () {
+    console.log(chalk.red('Remove Node.js'))
+  }
+}).argv
+
+yargs(hideBin(process.argv)).command({
+  command: 'list',
+  describe: 'list all Node.js',
+  handler: function () {
+    console.log(chalk.underline('list all Node.js'))
+  }
+}).argv
+
+yargs(hideBin(process.argv)).command({
+  command: 'read',
+  describe: 'read Node.js',
+  handler: function () {
+    console.log(chalk.red('read Node.js'))
+  }
+}).argv
